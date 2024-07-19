da373b57e8da8d7e2b030720009032db8ce0d945
Ritik Keshari <80599040+Ritikkesari@users.noreply.github.com>
2024-07-19 18:09:10 +0000

    added app.py

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import gradio as gr
+from src.trasnformer import translate
 
+def gradio_translate(text):
+    translate_text = translate(text, source_lang = "en", target_lang = "es")
+
+input_text = gr.inputs.Textbox(label = "Enter text to translate to spanish")
+
+ouput_text = gr.outputs.Textbox(label = "Translated text")
+
+interface = gr.Interface(fn=gradio_translate, inputs=input_text, outputs=output_text, title="Text translation")
+
+if __name__ == "__main__":
+    interface.launch()
