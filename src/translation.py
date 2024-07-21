95375b60ff6fd1cefee2bf8d149da9b158c71625
Ritik Kesari <ritikkesari@Ritiks-MacBook-Air.local>
2024-07-22 01:57:12 +0530

    new changes

--- src/translation.py
+++ src/translation.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 # Function to translate text
 def translate(text, source_lang="en", target_lang="es"):
     # Load the translation pipeline for translation tasks
-    translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-es")
+    translator = pipeline("translation", model="deactiva")
 
     # Translate the text from source_lang to target_lang
     translated_text = translator(text, src_lang=source_lang, tgt_lang=target_lang)[0]['translation_text']
